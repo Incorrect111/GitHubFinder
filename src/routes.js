56a5d2920263ffa0e4135ff17d5d56c6381d669c
Incorrect <alexeev.andrey1987@gmail.com>
2020-10-26 11:26:07 +0300

    init commit

---
+++ src/routes.js
@@ -0,0 +1,28 @@
+//Default
+import Vue from 'vue'
+import Router from 'vue-router'
+
+Vue.use(Router)
+
+//Pages
+import Home from '@/pages/Home.vue'
+import NotFound from '@/pages/404.vue'
+
+
+//Routering
+
+export default new Router({
+    mode: 'history',
+    routes: [{
+            path: '/',
+            name: 'home',
+            component: Home
+        },
+
+        {
+            path: '*',
+            name: 'notFound',
+            component: NotFound
+        }
+    ]
+})
